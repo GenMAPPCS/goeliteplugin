24fc4d8a8b2c97004b2425cbd9f76ab622944c7c
apico <apico@23aac120-1128-46ae-97c9-9e165d6aae65>
2009-09-26 00:25:42 +0000

    initial import

---
+++ trunk/goeliteplugin/org/genmapp/goelite/GOElitePlugin.java
@@ -0,0 +1,18 @@
+package org.genmapp.goelite
+
+import javax.swing.*;
+
+import cytoscape.Cytoscape;
+import cytoscape.plugin.CytoscapePlugin
+
+public class GOElitePlugin extends CytoscapePlugin {
+
+    public GOElitePlugin() {
+       
+	JMenuItem item = new JMenuItem("Run GO-Elite");
+	JMenu pluginMenu = Cytoscape.getDesktop().getCyMenus().getMenuBar().getMenu("Plugins");
+	pluginMenu.add(item);
+    }
+
+}
+
